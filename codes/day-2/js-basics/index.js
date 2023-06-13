599d9a46e01033ac1a1dcb8b60e0504434498a59
Joydip <joydip.mondal@gmail.com>
2023-06-13 17:25:19 +0530

    assignment-3 uploaded

---
+++ codes/day-2/js-basics/index.js
@@ -0,0 +1,11 @@
+//presentation logic
+//1. print menu for calculation: add, subtract, multiply, divide
+//2. ask user for choice
+//3. ask user for two inputs
+//4. based on choice do the operation and print the o/p
+//5. the appliaction will ask user whether to continue or not
+//6. depending on whether to continue or not, application either will start from step-1 or terminate
+
+//business logic
+//create four functions for four calculations:
+//all of them will have two arguments and will return a number type value after calculation
--- codes/day-2/objects-js-app/index.js
+++ codes/day-2/objects-js-app/index.js
@@ -1,10 +1,11 @@
-var x = 10
-var y = 20
-var res = x + y
-console.log(res)
-this.message = 'hello'
+//local variables in gloal scope
+// var x = 10
+// var y = 20
+// var res = x + y
+// console.log(res)
+// this.message = 'hello'
 
-
+//constructor function
 function person(name, id, salary) {
     var info
     this.personName = name
@@ -15,18 +16,31 @@ function person(name, id, salary) {
         return info
     }
 }
-console.log(person.prototype)
-setTimeout(
-    function () {
-        console.log('this is async func')
-    }
-)
+//creating objects
 var anilObject = new person('anil', 1, 1000)
 var sunilObject = new person('sunil', 2, 2000)
-console.log(anilObject.print())
-console.log(sunilObject.print())
+//accessing value properties
+//using dot operator
+console.log(anilObject.personName)
+//through indexer
+console.log(anilObject['personSalary'])
 
-console.log(window)
+//accessing functions properties of the object
+console.log(anilObject.print())
+console.log(sunilObject['print']())
+
+//accessing prototype object of this function
+console.log(person.prototype)
+
+//calling an asynchronous method
+// setTimeout(
+//     function () {
+//         console.log('this is async func')
+//     }
+// )
+
+//printing window object
+//console.log(window)
 
 /**
  * //function main(){
