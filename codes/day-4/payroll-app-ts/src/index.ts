9cbd34b89f1fdb3f36f2502488ba9f06636d7cba
Joydip <joydip.mondal@gmail.com>
2023-06-15 20:48:37 +0530

    day-4 end of day upload

--- codes/day-4/payroll-app-ts/src/index.ts
+++ codes/day-4/payroll-app-ts/src/index.ts
@@ -1,20 +1,37 @@
-import { Developer } from "./models/developer"
-import { Employee } from "./models/employee"
-import { Hr } from "./models/hr"
+function save() {
+    const idInput = <HTMLInputElement>document.getElementById('txtId');
+    const id = Number(idInput.value)
 
-const anilDev = new Developer('anil', 2, 1000, 2000, 3000, 4000)
-const sunilDev = new Developer('sunil', 1, 1500, 2500, 3500, 4500)
+    const nameInput = <HTMLInputElement>document.getElementById('txtName');
+    const name = nameInput.value
 
-const maheshHr = new Hr('mahesh', 4, 1200, 2200, 3200, 4200)
-const sureshHr = new Hr('suresh', 3, 1300, 2300, 3300, 4300)
+    const basicInput = <HTMLInputElement>document.getElementById('txtBasic');
+    const basic = Number(basicInput.value)
 
-const people: Employee[] = []
-people.push(anilDev, sureshHr, maheshHr, sunilDev)
+    const daInput = <HTMLInputElement>document.getElementById('txtDa');
+    const da = Number(daInput.value)
 
-people.forEach(
-    (p) => console.log(p.calculateSalary())
-)
+    const hraInput = <HTMLInputElement>document.getElementById('txtHra');
+    const hra = Number(hraInput.value)
 
-function show() {
-    window.alert('hi...')
+    const extraInput = <HTMLInputElement>document.getElementById('txtExtra');
+    const extra = Number(extraInput.value)
+
+    const radioDev = <HTMLInputElement>document.getElementById('radioDeveloper');
+
+    const radioHr = <HTMLInputElement>document.getElementById('radioHr');
+
+    if (radioDev.checked) {
+
+    }
+    if (radioHr.checked) {
+
+    }
+
 }
+
+const btnObjct = document.getElementById('btnAdd');
+btnObjct.addEventListener(
+    'click',
+    save
+)
